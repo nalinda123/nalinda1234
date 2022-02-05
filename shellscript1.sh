54781edea6e10de941e6d38b34e8ea672364426a
nalinda123 <99086685+nalinda123@users.noreply.github.com>
2022-02-05 22:49:27 +0530

    Create shellscript1.sh

---
+++ shellscript1.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+echo " nalinda damith"
+cd -
+cd /home
+ls -l
+touch myshellscript.txt
+echo "this is my first shell"> myshellscript.txt
+echo "this is my first shell"> myshellscript2.tx
